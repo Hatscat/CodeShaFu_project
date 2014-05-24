c67dae17b60bfb581891bf8a67e9e7dee114022e
solfen <solfen@hotmail.com>
2014-05-24 16:33:59 +0200

    Refonte web design !

--- new_game.php
+++ new_game.php
@@ -3,50 +3,71 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
-		<meta charset="utf-8" />
-		<title>editor</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css"/>
-	</head>
-	<body> <!-- id="cleanScreen" to remove some stuffs -->
-		<canvas id="canvas">
-			This browser is not supported
-		</canvas>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Foundation | Welcome</title>
+	<link rel="stylesheet" href="css/foundation.css" />
+	<script src="js/vendor/modernizr.js"></script>
+</head>
 
-		<img id="banTelethon" src="img/banniere.jpg" alt="banniere telethon" />
+<body> <!-- id="cleanScreen" to remove some stuffs -->
 
-		<div id="container2">
-			<div id="text"></div>
+	<div class="row">
+		<div class="large-5 large-offset-4 columns">
+			<h1>New Map</h1>
 		</div>
+	</div>
 
-		<div id="chat">
-			<img id="chat" src="img/chat1.png" alt="un chat" />
+	<div class="row">
+		<div class="large-6 large-offset-3 columns">
+			<form>
+				<div class="row">
+					<div class="large-3 columns"
+						<label for="mapName" class="right inline" >Map Name :</label>
+					</div>
+
+					<div class="large-7 columns">
+						<input id="mapName" type="text" placeholder="nameOfTheYear" />
+					</div>
+				</div>
+
+				<br/>
+
+				<div class="row">
+					<div class="large-3 columns"
+						<label for="blueprint"  class="right inline" >Blueprint :</label>
+					</div>
+
+					<div class="large-7 columns">
+						<select id="blueprint">
+							<option value="none" selected>None</option>
+							<option value="loren">Husker</option>
+							<option value="ipsum">Starbuck</option>
+							<option value="conais">Hot Dog</option>
+							<option value="pas">Apollo</option>
+							<option value="la">Apollo</option>
+							<option value="suite">Apollo</option>
+						</select>
+					</div>
+				</div>
+			</form>
 		</div>
-
-		<div id="bubule"></div>
-
-		<div id="bubule2"></div>
-		
-		<div id="container3">
-			<div id="text2"></div>
+	</div>
+	<br/>
+	<div class="row">
+		<div class="large-3 large-centered columns">
+			<a href="editor.php" class="large radius round button">Get Started</a><br/>
 		</div>
-		
-		<div id="container">
-			<button class="button" id="run_button">RUN</button>
-			<button class="button" id="reset_button">RESET</button>
-			<input type="text" id="save_name" placeholder="ex: amazingName"/>
-			<button class="button" id="save_button">SAVE MAP</button>
-			<button class="button" id="load_button">LOAD MAP</button>
-			
-			<pre id="editor"></pre>
-		</div>
+	</div>
 
-	</body>
+	<script src="js/libs/vendor/jquery.js"></script>
+	<script src="js/libs/foundation.min.js"></script>
+	<script>
+	$(document).foundation();
+	</script>
 
-	<script src="js/jquery.min.js" type="text/javascript"></script>
-	<script src="js/ace-builds/src-noconflict/ace.js" type="text/javascript"></script>
-	<script src="js/main.js" type="text/javascript"></script>
-	<script src="js/content.js" type="text/javascript"></script>
-	<script src="js/runLoop.js" type="text/javascript"></script>
+</body>
+
 
 </html>
